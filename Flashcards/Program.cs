1b53ffd58fc8d75fc38b5bef39dd9b918486bcba
DLee211 <90346764+DLee211@users.noreply.github.com>
2023-12-31 23:28:06 -0500

    Setting up the database connection

--- Flashcards/Program.cs
+++ Flashcards/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using Microsoft.Data.SqlClient;
 
-Console.WriteLine("Hello, World!");
+namespace Flashcards
+{
+    class Program
+    {
+        private static string connectionString = @"Data Source = (localdb)\MSSQLLocalDB;Integrated Security=True";
+
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    // Open the connection
+                    connection.Open();
+
+                    table.CommandText = @"CREATE TABLE IF NOT EXISTS coding_tracker(
+                                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        Date TEXT, 
+                                        StartTime TEXT NOT NULL,
+                                        EndTime TEXT NOT NULL,
+                                        Duration INTEGER,
+                                        CHECK (StartTime < EndTime))";
+
+                    tableCmd.ExecuteNonQuery();
+
+
+                    Console.WriteLine("Connected to LocalDB successfully.");
+
+                    // Close the connection when done
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    // Handle any exceptions that occur during connection
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            } 
+        }
+    }
+}
