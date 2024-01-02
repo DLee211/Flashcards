77789d042c7b089d71af3d014d8f4ad1de797eee
DLee211 <90346764+DLee211@users.noreply.github.com>
2024-01-01 22:46:19 -0500

    Able to add stacks

--- Flashcards/Program.cs
+++ Flashcards/Program.cs
@@ -7,18 +7,14 @@ namespace Flashcards
     {
         private static string connectionString = @"Data Source = (localdb)\MSSQLLocalDB;Integrated Security=True";
 
-        static string createStacksTableSql = "IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Stacks')" +
-                                      "   CREATE TABLE Stacks (" +
-                                      "       StackId INT PRIMARY KEY," +
-                                      "       StackName NVARCHAR(50))";
+        static string createStacksTableSql = "CREATE TABLE Stacks (StackId INT IDENTITY(1,1) PRIMARY KEY,StackName NVARCHAR(50))";
 
         // Define the SQL command to create Flashcards table if it doesn't exist
-        static string createFlashcardsSql = "IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Flashcards')" +
-                                          "   CREATE TABLE Flashcards (" +
-                                          "       FlashcardId INT PRIMARY KEY," +
-                                          "       Question NVARCHAR(255)," +
-                                          "       Answer NVARCHAR(255)," +
-                                          "       StackId INT FOREIGN KEY REFERENCES Stacks(StackId))";
+        static string createFlashcardsSql = "   CREATE TABLE Flashcards (" +
+                                            "       FlashcardId INT PRIMARY KEY," +
+                                            "       Question NVARCHAR(255)," +
+                                            "       Answer NVARCHAR(255)," +
+                                            "       StackId INT FOREIGN KEY REFERENCES Stacks(StackId))";
 
         static void Main(string[] args)
         {
@@ -51,7 +47,9 @@ namespace Flashcards
                     // Handle any exceptions that occur during connection
                     Console.WriteLine("Error: " + ex.Message);
                 }
-            } 
+            }
+
+            UserInput.Input();
         }
     }
 }
---
+++ Flashcards/Stack.cs
@@ -0,0 +1,94 @@
+﻿using ConsoleTableExt;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Flashcards;
+
+public class Stack
+{
+    private static string connectionString = @"Data Source = (localdb)\MSSQLLocalDB;Integrated Security=True";
+
+    public static void ManageStacks()
+    {
+        Console.Clear();
+
+        ViewStacks();
+
+        Console.WriteLine("---------------------------");
+        Console.WriteLine("Input a current stack name");
+        Console.WriteLine("Or input 0 to exit input.");
+        Console.WriteLine("---------------------------");
+
+        string input = Console.ReadLine();
+        
+        string insertSQL = $"INSERT INTO Stacks(StackName) Values('{input}')";
+
+        switch (input)
+        {
+            case "0":
+                break;
+
+            default:
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(insertSQL, connection))
+                    {
+                        command.ExecuteNonQuery();
+                        Console.WriteLine("Stack Created!");
+                        Console.WriteLine("Press Enter to continue:");
+                        Console.ReadLine();
+                    }
+
+                    connection.Close();
+                }
+                break;
+        }
+        UserInput.Input();
+    }
+
+    private static void ViewStacks()
+    {
+        string GetStackNames = "SELECT StackName FROM Stacks;";
+
+        SqlCommand command;
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            using (command = new SqlCommand(GetStackNames, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+
+            var tableData = new List<List<object>> { };
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        List<object> rowData = new List<object>
+                        {
+                            reader.GetString(0)
+                        };
+
+                        tableData.Add(rowData);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No data found!");
+                }
+                
+                ConsoleTableBuilder
+                    .From(tableData)
+                    .WithTitle("Stacks ", ConsoleColor.Yellow, ConsoleColor.DarkGray)
+                    .WithColumn("Name")
+                    .WithFormat(ConsoleTableBuilderFormat.Alternative)
+                    .ExportAndWriteLine(TableAligntment.Left);
+            }
+        }
+    }
+}
