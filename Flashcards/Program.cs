bf64f1df299cab2c1e071995b655c319f6fd45d6
DLee211 <90346764+DLee211@users.noreply.github.com>
2024-01-01 21:33:38 -0500

    Created database

--- Flashcards/Program.cs
+++ Flashcards/Program.cs
@@ -7,25 +7,39 @@ namespace Flashcards
     {
         private static string connectionString = @"Data Source = (localdb)\MSSQLLocalDB;Integrated Security=True";
 
+        static string createStacksTableSql = "IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Stacks')" +
+                                      "   CREATE TABLE Stacks (" +
+                                      "       StackId INT PRIMARY KEY," +
+                                      "       StackName NVARCHAR(50))";
+
+        // Define the SQL command to create Flashcards table if it doesn't exist
+        static string createFlashcardsSql = "IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Flashcards')" +
+                                          "   CREATE TABLE Flashcards (" +
+                                          "       FlashcardId INT PRIMARY KEY," +
+                                          "       Question NVARCHAR(255)," +
+                                          "       Answer NVARCHAR(255)," +
+                                          "       StackId INT FOREIGN KEY REFERENCES Stacks(StackId))";
+
         static void Main(string[] args)
         {
-            using (var connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 try
                 {
                     // Open the connection
                     connection.Open();
 
-                    table.CommandText = @"CREATE TABLE IF NOT EXISTS coding_tracker(
-                                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                        Date TEXT, 
-                                        StartTime TEXT NOT NULL,
-                                        EndTime TEXT NOT NULL,
-                                        Duration INTEGER,
-                                        CHECK (StartTime < EndTime))";
-
-                    tableCmd.ExecuteNonQuery();
-
+                    using (SqlCommand command = new SqlCommand(createStacksTableSql, connection))
+                    {
+                        command.ExecuteNonQuery();
+                        Console.WriteLine("Stack table created successfully");
+                    }
+                    
+                    using (SqlCommand command = new SqlCommand(createFlashcardsSql, connection))
+                    {
+                        command.ExecuteNonQuery();
+                        Console.WriteLine("Flashcards table created successfully!");
+                    }
 
                     Console.WriteLine("Connected to LocalDB successfully.");
 
